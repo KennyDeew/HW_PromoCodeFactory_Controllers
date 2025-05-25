6f55f660922e1dea0d585c8663a7149ec3b27f1e
KennyDew <dubrovskiynv@gmail.com>
2025-05-25 16:19:12 +0300

    fixed LastName in CreateEmployeeRequest

--- PromoCodeFactory.WebHost/Request/CreateEmployeeRequest.cs
+++ PromoCodeFactory.WebHost/Request/CreateEmployeeRequest.cs
@@ -7,7 +7,7 @@ namespace PromoCodeFactory.WebHost.Request
     {
         public string FirstName { get; set; }
 
-        public string SecondName { get; set; }
+        public string LastName { get; set; }
 
         public string Email { get; set; }
 
